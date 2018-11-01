3a0af9a7df06411e5424574187c6821ad65e019b
Sergunia <av93210@gmail.com>
2018-11-01 21:13:55 +0300

    spacebars, functions_names, grammar, 1 big massive

--- minimalism_1.py
+++ minimalism_1.py
@@ -1,58 +1,55 @@
 import graphics as gr
 
-#The program draws Arjan Janssen's picture
+# The program draws Arjan Janssen's picture
 window = gr.GraphWin("Minimalism", 1200, 700)
+    
+
+def draw_janssen_picture():
+    draw_floor_plinth_wall()
+    draw_pictures()
 
 
-def DrawJanssenPicture():
-    DrawFloorPlinthWall()
-    DrawPictures()
-
-
-def DrawFloorPlinthWall():
+def draw_floor_plinth_wall():
     # FLOOR
-    DrawRectangle(0, 560, 1200, 140, 81, 81, 79)
+    draw_rectangle(0, 560, 1200, 140, 81, 81, 79)
     # PLINTH
-    DrawRectangle(0, 540, 1200, 20, 219, 219, 219)
+    draw_rectangle(0, 540, 1200, 20, 219, 219, 219)
     # WALL
-    DrawRectangle(0, 0, 1200, 540, 240, 240, 240)
+    draw_rectangle(0, 0, 1200, 540, 240, 240, 240)
 
 
-def DrawPictures():
-    # let's define coloures of three pictures
-    Picture1 = [(153,154,158), (180,182,181), (172,176,177), (214,214,214)]
-    Picture2 = [(65,67,66), (6,8,7), (157,158,160), (25,29,30)]
-    Picture3 = [(202,204,203), (213,213,213), (223,223,223), (23,27,26)]
-    Pictures = [Picture1, Picture2, Picture3]
+def draw_pictures():
+    # let's define colours of three pictures
+    pictures = [((153, 154, 158), (180, 182, 181), (172, 176, 177), (214, 214, 214)), ((65, 67, 66), (6, 8, 7), (157, 158, 160), (25, 29, 30)), ((202, 204, 203), (213, 213, 213), (223, 223, 223), (23, 27, 26))]
     
     # Set point (xo,yo): left corner of Picture1
     xo = 350
     yo = 180
-    DrawSomePictures(Pictures,xo,yo)
+    draw_some_pictures(pictures, xo, yo)
 
 
-def DrawSomePictures(pic,x,y):
+def draw_some_pictures(pic,x,y):
     """
         Рисует все элементы из Pictures с отступом 200 от предыдущего
     """ 
     for step in range (len(pic)):
-        DrawPicture(x, y, pic[step])
+        draw_picture(x, y, pic[step])
         x += 200
 
 
-def DrawPicture(xo, yo, pic):
+def draw_picture(xo, yo, pic):
     """
         Рисует одну картину:
         xо,yо - координаты правой нижней точки левого верхней чсти картины;
         pic - список, хранящий наборы цветов четырёх прямоугольников картины
     """
-    DrawRectangle(xo, yo, 50, 80, *pic[0])
-    DrawRectangle(xo+50, yo, 50, 80, *pic[1])
-    DrawRectangle(xo, yo+80, 50, 80, *pic[2])
-    DrawRectangle(xo+50, yo+80, 50, 80, *pic[3])
+    draw_rectangle(xo, yo, 50, 80, *pic[0])
+    draw_rectangle(xo+50, yo, 50, 80, *pic[1])
+    draw_rectangle(xo, yo+80, 50, 80, *pic[2])
+    draw_rectangle(xo+50, yo+80, 50, 80, *pic[3])
 
 
-def DrawRectangle(x, y, length, height, r, g, b):
+def draw_rectangle(x, y, length, height, r, g, b):
     """
         Рисует прямоугольник:
         x,y - координаты правой нижней точки прямоугольника;
@@ -61,12 +58,13 @@ def DrawRectangle(x, y, length, height, r, g, b):
         r,g,b - параметры его цвета
     """
     rectangle = gr.Rectangle(gr.Point(x, y), gr.Point(x+length, y+height))
-    rectangle.setFill(gr.color_rgb(r,g,b))
-    rectangle.setOutline(gr.color_rgb(r,g,b))
+    rectangle.setFill(gr.color_rgb(r, g, b))
+    rectangle.setOutline(gr.color_rgb(r, g, b))
     rectangle.draw(window)
 
 
-DrawJanssenPicture()
+draw_janssen_picture()
 
 window.getMouse()
 window.close()
+
