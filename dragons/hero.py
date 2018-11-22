d9e7c9114ec308f19070cf3ff260dab07065fd24
Sergunia <av93210@gmail.com>
2018-11-22 23:06:54 +0300

    игра работает, без троллей

---
+++ dragons/hero.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+# license: GPLv3
+from gameunit import *
+
+#FIXME:
+"""В этом файле должен быть описан класс героя, унаследованный от Attacker
+Герой должен иметь 100 поинтов здоровья, атаку 50, опыт 0, имя, задаваемое в конструкторе
+Метод attack должен получать атрибут target и уменьшать его здоровье на величину атаки.
+
+"""
+class Hero(Attacker):
+
+    def __init__ (self, name):
+        self._name = name
+        self._experience = 0
+        self._attack = 50
+        self._health = 100
+
+    # def attack (self, target):
+     #     target._health -= self.
